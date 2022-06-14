ef61b47102f2eafb84aa2baa08cbc5b007d32c51
Abdulazeez Olabode <azeezolabode010@gmail.com>
2022-06-14 21:46:41 +0100

    Admin on template

--- tips/payment.py
+++ tips/payment.py
@@ -10,32 +10,3 @@ class PaypalReturnView(TemplateView):
 
 class PaypalCancelView(TemplateView):
     template_name = 'paypal_cancel.html'
-
-
-
-# @receiver(valid_ipn_received)
-# def paypal_payment_received(sender, **kwargs):
-#     ipn_obj = sender
-#     if ipn_obj.payment_status == ST_PP_COMPLETED:
-#         # WARNING !
-#         # Check that the receiver email is the same we previously
-#         # set on the `business` field. (The user could tamper with
-#         # that fields on the payment form before it goes to PayPal)
-#         if ipn_obj.receiver_email != 'your-paypal-business-address@example.com':
-#             # Not a valid payment
-#             return
-
-#         # ALSO: for the same reason, you need to check the amount
-#         # received, `custom` etc. are all what you expect or what
-#         # is allowed.
-#         try:
-#             my_pk = ipn_obj.invoice
-#             mytransaction = MyTransaction.objects.get(pk=my_pk)
-#             assert ipn_obj.mc_gross == mytransaction.amount and ipn_obj.mc_currency == 'EUR'
-#         except Exception:
-#             pass
-#         else:
-#             mytransaction.paid = True
-#             mytransaction.save()
-#     # else:
-#     #     logger.debug('Paypal payment status not completed: %s' % ipn_obj.payment_status)
